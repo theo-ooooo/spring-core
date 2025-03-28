c73931f0da858bc2ef0277390eedd7bce302c345
Theo <kkw.theo@gmail.com>
2025-03-28 15:23:04 +0900

    feat: 주문과 할인 도메인 실행과 테스트

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -1,4 +1,8 @@
-package hello.core.member;
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
