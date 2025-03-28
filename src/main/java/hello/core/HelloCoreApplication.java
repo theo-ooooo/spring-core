caf687a70baeec481499d2c8cf2e5f5c1d019004
Theo <kkw.theo@gmail.com>
2025-03-28 14:23:14 +0900

    init

---
+++ src/main/java/hello/core/HelloCoreApplication.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloCoreApplication.class, args);
+    }
+
+}
