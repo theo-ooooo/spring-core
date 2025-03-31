1d28d367c5039b6e8628e3572dbfc9e4ace893e8
Theo <kkw.theo@gmail.com>
2025-03-31 16:52:38 +0900

    test: @Configuration과 바이트코드 조작의 마법

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -27,4 +27,11 @@ public class ConfigurationSingletonTest {
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
 
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+        System.out.println("bean.getClass() = " + bean.getClass());
+    }
 }
